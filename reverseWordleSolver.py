47145fc249fffc47eb8b6dada93914626e7d7b80
cnqso <wmkelly@umich.edu>
2022-12-13 00:13:00 -0500

    Finished the first working reverse solution algo

--- reverseWordleSolver.py
+++ reverseWordleSolver.py
@@ -1,9 +1,6 @@
 from orderByRarity import orderByRarity
 import os
-wordlestring = "GBBGBGYBGBGGGGG"
-score = 3
-solution = "THORN"
-wordle = []
+
 
 #break this terrible recursive function thing into functions,
 #one that checks if a letter is valid for the position, one that determines
@@ -12,21 +9,6 @@ wordle = []
 
 #Check if the program has already organized words by frequency
 #Check filesize to ensure it isn't just the result of a botched generation
-if os.path.exists("./mostCommonWords.txt") and (os.path.getsize("./mostCommonWords.txt") > 1000):
-    print("mostCommonWords.txt is already populated.")
-else:
-    orderByRarity()
-    
-
-#TODO add high bias words to top
-wordlestring = "GBBGBGYBGBGGGGG"
-score = 3
-solution = "THORN"
-wordle = []
-for x in range(score):
-  wordle.append(wordlestring[(5*x):(5*x+5)])
-
-reversesolution = ""
 
 
 def yellowSquareElligible(gridRow, answer, letter):
@@ -48,8 +30,6 @@ def yellowSquareElligible(gridRow, answer, letter):
         
 
 
-
-
 def checkLine(gridRow, answer, word):
     for i in range(5):
         #print(f"Checking {word[i]} against {answer[i]} for a {gridRow[i]} square")
@@ -57,6 +37,7 @@ def checkLine(gridRow, answer, word):
             case "G":
                 if word[i] != answer[i]:
                     return False
+
             case "Y":
                 if (word[i] not in answer) or (word[i] == answer[i]):
                     return False
@@ -65,53 +46,74 @@ def checkLine(gridRow, answer, word):
                     return False
 
             case "B":
+                #Just check if it can be a yellow or a green. If not it's a blank, right?
                 if word[i] == answer[i]:
                     return False
 
-
-
+                if (word[i] in answer) and (yellowSquareElligible(gridRow, answer, word[i]) == True):
                     return False
-                #Just check if it can be a yellow or a green. If not it's a blank, right?
     else:
         return True
 
 # #Test cases
 # print(checkLine("GGGGG","GRUNK","GRUNK")) #True
-# print(checkLine("GGGGG","GRUNK","GRUNG")) #False
-
+# print(checkLine("BBBBB","PRATE","QWYUI")) #True
+# print(checkLine("GGBBB","EETTF","EEEEQ")) #True
 # print(checkLine("YYYYY","PAERT","APTER")) #True
 # print(checkLine("YYGYY","AABCC","CCBAA")) #True 
-
-print(checkLine("GGGGY","GRUNK","GRUNG")) #False
-print(checkLine("BBBBB","PRATE","APTER")) #False
-# print(checkLine("BBBBB","PRATE","QWYUI")) #True
-
-# print(checkLine("GGBBB","EETTF","EEETQ")) #True
-
-
-print
+# print(checkLine("GGGGG","GRUNK","GRUNG")) #False
+# print(checkLine("GGGGY","GRUNK","GRUNG")) #False
+# print(checkLine("BBBBB","PRATE","APTER")) #False
 
 
 
 
+def solveRow(wordleRow, answer):
+    possibleAnswers = []
+    if wordleRow == "GGGGG":
+        possibleAnswers.append(answer)
+        return possibleAnswers
+    with open("mostCommonWords.txt", "r") as validwordstext:
+        validwords = validwordstext.read().splitlines()
+        #TODO Append (pop?) biased words
+
+        for wordNumber in range(len(validwords)):
+            if checkLine(wordleRow, answer, validwords[wordNumber]) == True:
+                possibleAnswers.append(validwords[wordNumber])
+                
+        
+    #print(possibleAnswers)
+    return possibleAnswers
 
 
 
 
+if os.path.exists("./mostCommonWords.txt") and (os.path.getsize("./mostCommonWords.txt") > 1000):
+    print("mostCommonWords.txt is already populated.")
+else:
+    orderByRarity()
+    
+
+#TODO add high bias words to top
+def reverseSolveWordle (wordlestring, solution):
+    wordlestring = "GBBGBGYBGBGGGGG"
+    solution = "THORN"
+    score = len(wordlestring) // 5
+    wordle = []
+    for x in range(score):
+        wordle.append(wordlestring[(5*x):(5*x+5)])
+
+    reversesolution = ""
 
 
+    for row in wordle:
+        newAnswers = solveRow(row, solution)
+        reversesolution += newAnswers[0]
+
+    return reversesolution
 
 
-
-
-
-
-
-
-
-
-
-
+print(reverseSolveWordle("GBBGBGYBGBGGGGG", "THORN"))
 
 
 
@@ -159,12 +161,6 @@ print
                 
 
                 
-
-
-
-
-
-
 
 
     #sort valid wordle words in terms of commonality
